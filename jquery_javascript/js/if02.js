7a7f1432e3223f97e7c9c2d214c9a6385954b161
onhope <chch4934@gmail.com>
2024-02-09 01:06:43 +0900

    조건 02

---
+++ jquery_javascript/js/if02.js
@@ -0,0 +1,93 @@
+// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
+// 조건 : 넘버링을 해주세요. EX : 참2, 참3, 거짓2, 거짓3 ...
+
+console.clear();
+
+if (true) {
+  console.log("참");
+}
+
+if (false) {
+  console.log("거짓");
+}
+
+var a = 10;
+
+// `==` => 같다.
+if ( a == 10 ) {
+  console.log("참2");
+}
+
+// `!=` => 같지 않다.
+if ( a != 10 ) {
+  console.log("거짓2");
+}
+
+if ( a > 10 ) {
+  console.log("거짓3");
+}
+
+if ( a >= 10 ) {
+  console.log("참3");
+}
+
+var b = 10;
+
+if ( a == b ) {
+  console.log("참4");
+}
+
+// boolean c => c 에는 오직 true/false 만 담을 수 있다.
+var c = a != b;
+
+if ( c ) {
+  console.log("거짓4");
+}
+
+if ( c == false ) {
+  console.log("참5");
+}
+
+// `!` => 반전
+if ( !c ) {
+  console.log("참6");
+}
+
+// `!` => 반전
+if ( !(!c) ) {
+  console.log("거짓5");
+}
+
+var d = true;
+
+if ( c != d ) {
+  console.log("참7");
+}
+
+if ( c && d ) {
+  console.log("거짓6");
+}
+
+if ( c || d ) {
+  console.log("참8");
+}
+
+if ( c == false && d ) {
+  console.log("참9");
+}
+
+if ( c || c ) {
+  console.log(" 거짓7");
+}
+
+if ( false || false ) {
+  console.log("거짓8");
+}
+
+if ( 1 == 1 && 2 == 2 && 3 == 3 && 4 == 5 ) {
+  console.log("거짓9");
+}
+
+if ( 1 == 1 && 2 == 2 && 3 == 3 && 4 == 5 || true ) {
+  console.log("참10");
+}
