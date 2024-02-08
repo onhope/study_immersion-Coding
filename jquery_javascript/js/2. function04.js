a9da8eeb0c4898694bc9f218b2153d1e722e07ce
onhope <chch4934@gmail.com>
2024-02-09 02:05:23 +0900

    2. 함수 반환

---
+++ jquery_javascript/js/2. function04.js
@@ -0,0 +1,16 @@
+console.clear();
+
+// return은 함수 실행시 반환하는 값으로, 데이터가 나가는 통로라고 생각을 하면 된다. 
+function plus(a, b) {
+    return a + b;
+}
+
+console.log(plus(10, 20)); // 30
+console.log(plus(100, 200)); // 300
+
+function minus(a, b) {
+    return a - b;
+}
+
+console.log(minus(10, 20)); // -10
+console.log(minus(100, 200)); // -100
