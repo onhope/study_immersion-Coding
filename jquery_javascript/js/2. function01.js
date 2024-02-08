c45a648307ec601385d23c7f5d9c7603614edc6b
onhope <chch4934@gmail.com>
2024-02-09 01:56:51 +0900

    2. 함수

---
+++ jquery_javascript/js/2. function01.js
@@ -0,0 +1,26 @@
+// 문제 : 함수 3개를 정의하고 실행해주세요.
+
+console.clear();
+
+var 함수1 = function() {
+    console.log("함수1 실행됨");
+};
+
+함수1();
+
+var 함수2 = function() {
+    console.log("함수2 실행됨");
+};
+
+함수2();
+
+function 함수3() {
+    console.log("함수3 실행됨");
+}
+
+함수3();
+
+
+
+
+
