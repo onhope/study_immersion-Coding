32f5b284598ecc12c48d880ab191d15cb51dacc2
onhope <chch4934@gmail.com>
2024-02-10 23:54:38 +0900

    3. 객체

---
+++ jquery_javascript/js/3. objext.js
@@ -0,0 +1,29 @@
+console.clear();
+/*
+var person1__name = "홍길동";
+var person1__age = 22;
+
+var person2__name = "홍길순";
+var person2__age = 25;
+*/
+/*
+var person1 = {};
+person1.name = "홍길동";
+person1.age = 22;
+console.log(person1);
+*/
+
+/*
+var person1 = {};
+person1["name"] = "홍길동";
+person1["age"] = 22;
+console.log(person1);
+*/
+
+var person1 = {
+  "name":"홍길동",
+  "age":22
+};
+person1.gender = "남";
+person1.age++;
+console.log(person1);
